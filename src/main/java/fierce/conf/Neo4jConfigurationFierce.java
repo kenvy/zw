ce38f5942c9f177c626ec3c6a3162f291f28481e
zhangwei <yaoyaolingma@126.com>
2016-04-29 21:40:06 +0800

    Pencho !

--- src/main/java/fierce/conf/Neo4jConfigurationFierce.java
+++ src/main/java/fierce/conf/Neo4jConfigurationFierce.java
@@ -3,6 +3,7 @@ package fierce.conf;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 
