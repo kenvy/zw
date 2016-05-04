71086eb206047a2e23601281fc6cc3952ec6a8df
zhangwei <yaoyaolingma@126.com>
2016-05-04 14:41:47 +0800

    Pencho !

--- src/main/java/fierce/Application.java
+++ src/main/java/fierce/Application.java
@@ -1,11 +1,7 @@
 package fierce;
 
-import fierce.service.impl.GreetingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by zw on 2016/4/21
