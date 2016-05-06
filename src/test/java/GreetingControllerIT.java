1329b473b05dbde77788825b2307a95d1e2f2f9a
zhangwei <yaoyaolingma@126.com>
2016-05-06 16:51:43 +0800

    Pencho !

---
+++ src/test/java/GreetingControllerIT.java
@@ -0,0 +1,48 @@
+
+
+import fierce.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URL;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by zw on 2016/5/6
+ * Email: yaoyaolingma@126.com
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest({"server.port=0"})
+public class GreetingControllerIT {
+
+    @Value("${local.server.port}")
+    private int port;
+
+    private URL base;
+    private RestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/greeting");
+        template = new TestRestTemplate();
+    }
+
+    @Test
+    public void getGreet() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        assertThat(response.getBody(), equalTo("{\"id\":1,\"content\":\"Hello, World!\"}"));
+    }
+}
