cefdc8bf0a542be599ec08cf6874969fb6c60adb
go <yaoyaolingma@gmail.com>
2016-05-08 14:05:08 +0800

    scheduled task

---
+++ src/main/java/fierce/conf/ScheduledConfiguration.java
@@ -0,0 +1,11 @@
+package fierce.conf;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by Fierce on 2016/5/8
+ * Email: yaoyaolingma@126.com
+ */
+@EnableScheduling
+public class ScheduledConfiguration {
+}
---
+++ src/main/java/fierce/service/ScheduledTasks.java
@@ -0,0 +1,21 @@
+package fierce.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Fierce on 2016/5/8
+ * Email: yaoyaolingma@126.com
+ */
+@Component
+public class ScheduledTasks {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println("The time is now " + dateFormat.format(new Date()));
+    }
+}
