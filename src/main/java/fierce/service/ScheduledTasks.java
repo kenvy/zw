495c62477b84abc1115e385a3edc14432a100e0d
zhangwei <yaoyaolingma@126.com>
2016-05-10 10:42:45 +0800

    Pencho !

--- src/main/java/fierce/service/ScheduledTasks.java
+++ src/main/java/fierce/service/ScheduledTasks.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class ScheduledTasks {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 5000*12)
     public void reportCurrentTime() {
         System.out.println("The time is now " + dateFormat.format(new Date()));
     }
