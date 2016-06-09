a8ee8aff2b67bbac0db3316effd1492323a83923
go <yaoyaolingma@gmail.com>
2016-06-09 10:28:10 +0800

    Application comments for nero4j.

--- src/main/java/fierce/Application.java
+++ src/main/java/fierce/Application.java
@@ -14,11 +14,14 @@ import java.io.IOException;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+
+        //Demo for nero4j.[start]
         try {
             FileUtils.deleteRecursively(new File("target/hello.db"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //Demo for nero4j.[end]
 
         SpringApplication.run(Application.class, args);
     }
